0c7f18caef40113fbb493554c07cea0a0bfd5e8a
Jonas Schubert <guepardoapps@gmail.com>
2018-11-19 17:04:02 +0100

    CodingDojo 19.11.2018

---
+++ snippets/snippets/Array/Array.DropWhile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace snippets.Array
+{
+    /// <summary>
+    /// Partial class for array snippets
+    /// </summary>
+    public static partial class SnippetsArray
+    {
+        public static IEnumerable<T> DropWhile<T>(this IEnumerable<T> list, Func<T, bool> filter)
+        {
+            if (list == null)
+            {
+                throw new ArgumentNullException(nameof(list));
+            }
+
+            if (filter == null)
+            {
+                throw new ArgumentNullException(nameof(filter));
+            }
+
+            var continueToEnd = false;
+
+            foreach (var element in list)
+            {
+                if (!continueToEnd && !filter(element))
+                {
+                    continue;
+                }
+
+                continueToEnd = true;
+
+                yield return element;
+            }
+
+            yield break;
+        }
+    }
+}
