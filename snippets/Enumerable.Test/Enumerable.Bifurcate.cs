3dfe1de3c1567602d9aaaff4cc71b11ddfff8244
Jonas Schubert <guepardoapps@gmail.com>
2018-11-21 18:27:34 +0100

    implement snippet Enumerable.Bifurcate

---
+++ snippets/Enumerable.Test/Enumerable.Bifurcate.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace conplement.snippets.Enumerable.Test
+{
+    public class EnumerableBifurcateUnitTest
+    {
+        [Fact]
+        public void ReturnArray1EmptyAndArray2Full_WhenTheFilterAlwaysMeet()
+        {
+            // Arrange
+            var list = new List<int>
+            {
+                1, 2, 3, 4, 1
+            };
+
+            // Act
+            (IEnumerable<int> actual1, IEnumerable<int> actual2) = list.Bifurcate(x => x > 0);
+
+            // Assert
+            actual1.Should().NotBeNull();
+            actual1.Count().Should().Be(5);
+            actual1.Should().BeEquivalentTo(list);
+            actual2.Should().NotBeNull();
+            actual2.Count().Should().Be(0);
+        }
+
+        [Fact]
+        public void ReturnArray1FullAndArray2Empty_WhenTheFilterNeverMeets()
+        {
+            // Arrange
+            var list = new List<string>
+            {
+                "Hello","world","organisation","seconds","of"
+            };
+
+            // Act
+            (IEnumerable<string> actual1, IEnumerable<string> actual2) = list.Bifurcate(x => !x.Contains("o"));
+
+            // Assert
+            actual1.Should().NotBeNull();
+            actual1.Count().Should().Be(0);
+            actual2.Should().NotBeNull();
+            actual2.Count().Should().Be(5);
+            actual2.Should().BeEquivalentTo(list);
+        }
+
+        [Fact]
+        public void ReturnExpectedSplittedArrays_WhenTheFilterPartiallyMeets()
+        {
+            // Arrange
+            var list = new List<bool>
+            {
+                false, true, false, false, false, false, false, false, true
+            };
+
+            // Act
+            (IEnumerable<bool> actual1, IEnumerable<bool> actual2) = list.Bifurcate(x => x);
+
+            // Assert
+            actual1.Should().NotBeNull();
+            actual1.Count().Should().Be(2);
+            actual2.Should().NotBeNull();
+            actual2.Count().Should().Be(7);
+        }
+
+        [Fact]
+        public void ShouldWorkForComplexTypes()
+        {
+            // Arrange
+            var list = new List<Person>
+            {
+                new Person { Name = "Tina" },
+                new Person { Name = "Thomas" },
+                new Person { Name = "Annabelle" },
+                new Person { Name = "John" },
+                new Person { Name = "Richard" }
+            };
+
+            // Act
+            (IEnumerable<Person> actual1, IEnumerable<Person> actual2) = list.Bifurcate(x => x.Name.StartsWith("T"));
+
+            // Assert
+            actual1.Should().NotBeNull();
+            actual1.Count().Should().Be(2);
+            actual2.Should().NotBeNull();
+            actual2.Count().Should().Be(3);
+        }
+
+        [Fact]
+        public void ShouldThrowException_WhenNullEnumerable_IsGiven()
+        {
+            // Arrange
+            List<int> list = null;
+
+            // Act
+            Action action = () => list.Bifurcate(x => x > 2);
+
+            // Assert
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void ShouldThrowException_WhenNullFilter_IsGiven()
+        {
+            // Arrange
+            var list = new List<int>
+            {
+                1, 2, 3, 4, 1
+            };
+
+            // Act
+            Action action = () => list.Bifurcate(null);
+
+            // Assert
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        private class Person
+        {
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ snippets/Enumerable/Enumerable.Bifurcate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace conplement.snippets.Enumerable
+{
+    /// <summary>
+    /// Partial class for Enumerable snippets
+    /// </summary>
+    public static partial class Enumerable
+    {
+        /// <summary>
+        /// Splits values into two groups. If an element in filter is truthy, the corresponding element in the collection belongs to the first group; otherwise, it belongs to the second group.
+        /// </summary>
+        public static (IEnumerable<T>, IEnumerable<T>) Bifurcate<T>(this IEnumerable<T> list, Func<T, bool> filter)
+        {
+            if (list == null)
+            {
+                throw new ArgumentNullException(nameof(list));
+            }
+
+            if (filter == null)
+            {
+                throw new ArgumentNullException(nameof(filter));
+            }
+
+            IEnumerable<T> enumerable1 = list.Where(filter);
+            IEnumerable<T> enumerable2 = list.Where(x => !enumerable1.Any(y => y.Equals(x)));
+
+            return (enumerable1, enumerable2);
+        }
+    }
+}
