6a1475b001712af20816b5c1dfe5a188dc1e6df8
Jonas Schubert <guepardoapps@gmail.com>
2018-11-27 18:48:23 +0100

    Implemented better AllEqual suggested by @x3lor

--- snippets/Enumerable/Enumerable.AllEqual.cs
+++ snippets/Enumerable/Enumerable.AllEqual.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace conplement.snippets.Enumerable
+namespace Conplement.Snippets.Enumerable
 {
     /// <summary>
     /// Partial class for Enumerable snippets
@@ -10,16 +10,33 @@ namespace conplement.snippets.Enumerable
     public static partial class Enumerable
     {
         /// <summary>
-        /// Check if all elements in an array are equal.
+        /// Check if all elements in an enumerable are equal.
         /// </summary>
-        public static bool AllEqual<T>(this IEnumerable<T> list) where T : struct
+        public static bool AllEqual<T>(this IEnumerable<T> enumerable)
         {
-            if (list == null)
+            if (enumerable == null)
             {
-                throw new ArgumentNullException(nameof(list));
+                throw new ArgumentNullException(nameof(enumerable));
             }
 
-            return list.All(entry => list.First().Equals(entry));
+            var testList = enumerable.ToList();
+            if (testList.Count <= 1)
+            {
+                return true;
+            }
+
+            var compareObject = testList[0];
+            for (int index = 0; index < testList.Count; index++)
+            {
+                var entry = testList[index];
+                if ((entry == null || !entry.Equals(compareObject))
+                    && !(entry == null && compareObject == null))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
