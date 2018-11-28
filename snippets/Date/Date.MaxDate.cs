df713723bd147941844a6e637cb9430599686ffe
Jonas Schubert <guepardoapps@gmail.com>
2018-11-28 17:03:17 +0100

    implement snippet Date.MaxDate #28

---
+++ snippets/Date/Date.MaxDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Conplement.Snippets.Date
+{
+    /// <summary>
+    /// Partial class for date snippets
+    /// </summary>
+    public static partial class Date
+    {
+        /// <summary>
+        /// Returns the maximum of the given dates.
+        /// </summary>
+        public static DateTime MaxDate(params DateTime[] dateTimeList)
+        {
+            if (dateTimeList == null || !dateTimeList.Any())
+            {
+                throw new ArgumentException("The dateTimeList may not be empty!", nameof(dateTimeList));
+            }
+
+            if (!dateTimeList.All(x => dateTimeList.First().Kind == x.Kind))
+            {
+                throw new ArgumentException("All params have to have the same timezone!", nameof(dateTimeList));
+            }
+
+            return dateTimeList.Max();
+        }
+    }
+}
