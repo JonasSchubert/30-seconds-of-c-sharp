bfb5a6c75ac5a789cc1901f43716541ffd576646
Jonas Schubert <guepardoapps@gmail.com>
2019-05-19 21:06:14 +0200

    implements Enumerable.CountOccurences

---
+++ snippets/Enumerable/Enumerable.CountOccurences.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Conplement.Snippets.Enumerable
+{
+    /// <summary>
+    /// Partial class for Enumerable snippets
+    /// </summary>
+    public static partial class Enumerable
+    {
+        /// <summary>
+        /// Counts the occurrences of a value in an enumerable.
+        /// Basically the same as implemented in System.Linq:
+        /// public static int Count<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate);
+        /// </summary>
+        public static int CountOccurences<T>(this IEnumerable<T> enumerable, T value)
+        {
+            if (enumerable == null)
+            {
+                throw new ArgumentNullException(nameof(enumerable));
+            }
+
+            return enumerable.Count(x => x.Equals(value));
+        }
+    }
+}
