57609be9651b6eba109bbcc29b95de172b636af0
Jonas Schubert <guepardoapps@gmail.com>
2019-05-29 18:09:52 +0200

    implements Enumerable.FindLast #68

---
+++ snippets/Enumerable.Test/Enumerable.FindLast.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Conplement.Snippets.Enumerable.Test
+{
+    public class EnumerableFindLastUnitTest
+    {
+        [Fact]
+        public void ReturnExpected_ForListsOfInt()
+        {
+            // Arrange
+            var list = new List<int> { 1, 2, 3, 4, 0 };
+
+            // Act
+            var actual = list.FindLast(x => x % 4 == 0 && x != 0);
+
+            // Assert
+            actual.Should().Be(4);
+        }
+
+        [Fact]
+        public void ReturnExpected_ForArraysOfString()
+        {
+            // Arrange
+            var array = new string[] { "Hello", "world", "organisation", "seconds", "of" };
+
+            // Act
+            var actual = array.FindLast(x => x.Contains("or"));
+
+            // Assert
+            actual.Should().Be("organisation");
+        }
+
+        [Fact]
+        public void ReturnExpected_ForListOfBoolean()
+        {
+            // Arrange
+            var list = new List<bool?> { false, false, false, false, false, false, false, false, false };
+
+            // Act
+            var actual = list.FindLast(x => x == true);
+
+            // Assert
+            actual.Should().Be(null);
+        }
+
+        [Fact]
+        public void ReturnExpected_ForArrayOfComplex()
+        {
+            // Arrange
+            var list = new List<Person> { new Person { Name = "Tina" }, new Person { Name = "Thomas" }, new Person { Name = "Annabelle" }, new Person { Name = "Tina" } };
+
+            // Act
+            var actual = list.FindLast(x => x.Name.StartsWith("T"));
+
+            // Assert
+            actual.Should().Be(list.ElementAt(3));
+        }
+
+        [Fact]
+        public void ShouldThrowException_WhenNullEnumerable_IsGiven()
+        {
+            // Arrange
+            double[] array = null;
+
+            // Act
+            Action action = () => array.FindLast(x => x == 0);
+
+            // Assert
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void ShouldThrowException_WhenNullWhereFunction_IsGiven()
+        {
+            // Arrange
+            var list = new List<uint> { 1, 2, 3, 4 };
+
+            // Act
+            Action action = () => list.FindLast(null);
+
+            // Assert
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        private class Person
+        {
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ snippets/Enumerable/Enumerable.FindLast.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Conplement.Snippets.Enumerable
+{
+    /// <summary>
+    /// Partial class for Enumerable snippets
+    /// </summary>
+    public static partial class Enumerable
+    {
+        /// <summary>
+        /// Returns the last element for which the provided function returns a truthy value.
+        /// </summary>
+        public static T FindLast<T>(this IEnumerable<T> enumerable, Func<T, bool> whereFunction)
+        {
+            if (enumerable == null)
+            {
+                throw new ArgumentNullException(nameof(enumerable));
+            }
+
+            if (whereFunction == null)
+            {
+                throw new ArgumentNullException(nameof(whereFunction));
+            }
+
+            return enumerable.Where(whereFunction).Reverse().FirstOrDefault();
+        }
+    }
+}
