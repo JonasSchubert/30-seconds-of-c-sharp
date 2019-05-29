4e668f22bb223102fb0bcfb647c400a12bef9323
Jonas Schubert <guepardoapps@gmail.com>
2019-05-29 17:02:32 +0200

    implements Enumerable.HasDuplicates #60

---
+++ snippets/Enumerable/Enumerable.HasDuplicates.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Conplement.Snippets.Enumerable
+{
+    /// <summary>
+    /// Partial class for Enumerable snippets
+    /// </summary>
+    public static partial class Enumerable
+    {
+        /// <summary>
+        /// Checks an enumerable for duplicate values. Returns true if duplicate values exist and false if values are all unique.
+        /// </summary>
+        public static bool HasDuplicates<T>(this IEnumerable<T> enumerable) =>
+            enumerable == null
+                ? throw new ArgumentNullException(nameof(enumerable))
+                : enumerable.Count() != enumerable.Distinct().Count();
+    }
+}
