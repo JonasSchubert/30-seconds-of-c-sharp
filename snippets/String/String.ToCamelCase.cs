87e75c559a36002547fbe788123c552eb14fb227
Jonas Schubert <jonas.schubert.projects@web.de>
2019-12-01 18:43:18 +0100

    updates ToCamelCase

--- snippets/String/String.ToCamelCase.cs
+++ snippets/String/String.ToCamelCase.cs
@@ -13,7 +13,7 @@ namespace JonasSchubert.Snippets.String
         /// </summary>
         public static string ToCamelCase(this string input)
         {
-            var value = string.Join("", Regex.Matches(input, @"/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g")
+            var value = string.Join(string.Empty, Regex.Matches(input, @"/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g")
                 .Select(x => $"{x.Value.First().ToString().ToUpper()}{x.Value.Substring(1).ToLower()}"));
             return char.ToLowerInvariant(value[0]) + value.Substring(1);
         }
